1151a7f2e78b56137ea9c3815ad02e3e5165935e
dallin91 <dallinreeves@gmail.com>
2019-04-09 13:00:28 -0500

    added game intro

--- adventuregame.py
+++ adventuregame.py
@@ -13,3 +13,21 @@ rm1 = ['dice', 'axe', 'knife', 'artifact', 'gas can']
 leftrm = ['painting', 'spiderweb', 'torch']
 rightrm = ['skeleton', 'lighter']
 forwardrm = [something]
+
+print("You find yourself alone in the forest, late at night")
+input('press enter to continue')
+print("Nothing but the cool night air and darkness surround you")
+input('press enter...')
+print("You meander through the woods, trying to find any sign of civilization")
+input('press enter...')
+print('You approach a large, dark structure')
+begin = input("Do you enter? (y/n) ")
+
+if begin.lower() == 'n':
+    print('An arrow comes flying through the air, piercing your heart')
+    input('press enter...')
+    print('As you lay on the ground, taking your final breaths, you wish you entered that building')
+    exit()
+else:
+    print('You bravely take your first steps into the structure')
+    

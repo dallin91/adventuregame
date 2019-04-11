187a1d780f603510b350a25e9a31c5dacdaccf6a
dallin91 <dallinreeves@gmail.com>
2019-04-10 19:44:46 -0500

    added content

--- adventuregame.py
+++ adventuregame.py
@@ -14,25 +14,35 @@ leftrm = ['painting', 'spiderweb', 'torch']
 rightrm = ['skeleton', 'lighter']
 forwardrm = [something]
 
-print("You find yourself alone in the forest, late at night")
+print("\nYou find yourself alone in the forest, late at night")
 input('press enter to continue')
-print("Nothing but the cool night air and darkness surround you")
+print("\nNothing but the cool night air and darkness surround you")
 input('press enter...')
-print("You meander through the woods, trying to find any sign of civilization")
+print("\nYou meander through the woods, trying to find any sign of civilization")
 input('press enter...')
-print('You approach a large, dark structure')
+print('\nYou approach a large, dark structure')
 begin = input("Do you enter? (y/n) ")
 
 if begin.lower() == 'n':
-    print('An arrow comes flying through the air, piercing your heart')
+    print('\nAn arrow comes flying through the air, piercing your heart')
     input('press enter...')
-    print('As you lay on the ground, taking your final breaths, you wish you entered that building')
+    print('\nAs you lay on the ground, taking your final breaths, you wish you entered that building')
     exit()
 else:
-    print('You bravely take your first steps into the structure')
+    print('\nYou bravely take your first steps into the structure')
+    input('press enter...')
 
-print('You step into a dark room')
+print('\nYou step into a dark room')
 input('press enter...')
-print('The moodnlight shines in through an old, broken window, illuminating the room')
+print('\nThe moonlight shines in through an old, broken window, illuminating the room')
 input('press enter...')
-    
+print('\nYou notice 3 doors: straight ahead, to the left, and to the right')
+input('press enter...')
+print('\nYou also notice several items scattered throughout the room')
+action1 = input('\nDo you want to look around the room, \nor go to one of the other rooms (look, right, left, or straight)? ')
+
+if action1.lower() == 'look':
+    print('\nYou take a closer look at the items on the floor')
+    input('press enter...')
+    print('\nLeaning against the wall, covered in dust, you find an ' + rm1[1])
+    input('press enter...')
